d6d7294e7fadf79b8251eddf62e3ee3e423bd9f1
jaylordranjolapena1 <nicodave1217@gmail.com>
2026-01-08 16:55:15 +0800

    Update uid.js

--- Jaylord/commands/uid.js
+++ Jaylord/commands/uid.js
@@ -1,35 +1,30 @@
-module.exports.config = {
-  name: "uid",
-  version: "1.0.1",
-  hasPermssion: 0,
-  credits: "ChatGPT",
-  description: "Show Facebook user ID",
-  commandCategory: "Utility",
-  usages: "[mention/reply/userID]",
-  cooldowns: 3
-};
+module.exports = {
+  config: {
+    name: "uid",
+    version: "1.0.2",
+    hasPermssion: 0,
+    credits: "ChatGPT",
+    description: "Show Facebook user ID",
+    commandCategory: "Utility",
+    usages: "/uid [mention/reply/userID]",
+    cooldowns: 2
+  },
 
-module.exports.run = async function ({ api, event, args }) {
-  const { threadID, messageID, senderID, mentions, messageReply } = event;
+  run: async function ({ api, event, args }) {
+    const { threadID, messageID, senderID, mentions, messageReply } = event;
 
-  let targetID = senderID;
+    let targetID = senderID;
 
-  // If user mentioned someone
-  if (mentions && Object.keys(mentions).length > 0) {
-    targetID = Object.keys(mentions)[0];
+    if (Object.keys(mentions || {}).length > 0) {
+      targetID = Object.keys(mentions)[0];
+    }
+    else if (messageReply?.senderID) {
+      targetID = messageReply.senderID;
+    }
+    else if (args[0] && !isNaN(args[0])) {
+      targetID = args[0];
+    }
+
+    return api.sendMessage(`🆔 User ID:\n${targetID}`, threadID, messageID);
   }
-  // If replying to someone
-  else if (messageReply && messageReply.senderID) {
-    targetID = messageReply.senderID;
-  }
-  // If user typed an ID
-  else if (args[0] && !isNaN(args[0])) {
-    targetID = args[0];
-  }
-
-  return api.sendMessage(
-    `🆔 User ID:\n${targetID}`,
-    threadID,
-    messageID
-  );
 };
