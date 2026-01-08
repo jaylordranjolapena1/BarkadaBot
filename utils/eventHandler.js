1506e44b122bbd577b5f7f045bd02dedb521a42c
jaylordranjolapena1 <nicodave1217@gmail.com>
2026-01-09 00:43:47 +0800

    Update eventHandler.js

--- utils/eventHandler.js
+++ utils/eventHandler.js
@@ -1,6 +1,10 @@
 module.exports = async function ({ api, event }) {
+  const realType = event.type;
+
+  console.log("📥 EVENT:", realType);
+
   for (const ev of global.client.events.values()) {
-    if (!ev.config.eventType.includes(event.logMessageType || event.type)) continue;
+    if (!ev.config.eventType.includes(realType)) continue;
 
     try {
       await ev.run({ api, event });
