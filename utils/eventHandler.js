b8895fa08fb393714c192dc65734c6bc59158abe
jaylordranjolapena1 <nicodave1217@gmail.com>
2026-01-08 20:02:43 +0800

    Update eventHandler.js

--- utils/eventHandler.js
+++ utils/eventHandler.js
@@ -1,13 +1,12 @@
 module.exports = async function ({ api, event }) {
   const handlers = global.client.events.get(event.type);
-
   if (!handlers) return;
 
   for (const ev of handlers) {
     try {
       await ev.run({ api, event });
     } catch (e) {
-      console.error(`Event error [${ev.config.name}]`, e);
+      console.error("❌ Event error:", e);
     }
   }
 };
