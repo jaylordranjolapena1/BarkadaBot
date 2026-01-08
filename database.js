29db0234e88a8717698528c82faa5d0598a3b85b
jaylordranjolapena1 <nicodave1217@gmail.com>
2026-01-08 15:52:50 +0800

    Create database.js

---
+++ database.js
@@ -0,0 +1,27 @@
+const admin = require("firebase-admin");
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://mybot-d79df-default-rtdb.asia-southeast1.firebasedatabase.app/"
+  });
+}
+
+const db = admin.database();
+
+async function getData(path) {
+  const snap = await db.ref(path).once("value");
+  return snap.val();
+}
+
+async function setData(path, value) {
+  await db.ref(path).set(value);
+}
+
+async function deleteData(path) {
+  await db.ref(path).remove();
+}
+
+module.exports = { getData, setData, deleteData };
